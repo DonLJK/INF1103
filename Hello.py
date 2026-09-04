ab25e3660fd6a2513dda61fcb73d5ec4d8136c7f
DonLJK <donovanlimjk18.21@gmail.com>
2026-09-04 12:07:41 +0800

    fun or no fun

--- Hello.py
+++ Hello.py
@@ -1,9 +1,12 @@
-followers = 100
-followers += 50
-print("Day 1: Followers =", followers)
+username = input("Enter your username: ")
+age = int(input("Enter your age: "))
+category = input("Enter your category (e.g., student, professional): ")
 
-followers += 20
-print("Day 2: Followers =", followers)
+print("\nInstagram Profile")
+print("=======================")
+print("username: ", username)
+print("age: ", age)
+print("category: ", category)
 
-followers += 10
-print("Day 3: Followers =", followers)
+if age>40 and category == "fun":
+    print("You are eligible for the fun category!")
