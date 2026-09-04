f8cd995046a4cdb0ec41048affe835233004a5fe
DonLJK <donovanlimjk18.21@gmail.com>
2026-09-04 10:55:08 +0800

    Changes to Hello.py file

--- Hello.py
+++ Hello.py
@@ -1,12 +1,10 @@
-print("===============================")
-print("Welcome here")
-print("My first post!")
-print("===============================")
-
-username = "Cool_creator"
-bio = "Fun Blogger"
 followers = 100
 
-print("username: " + username)
-print("bio: " + bio)
-print("followers: " + str(followers))
+followers +=50
+print("Day 1: Followers =", followers)
+followers += 20
+print("Day 2: Followers =", followers)
+followers += 10
+print("Day 3: Followers =", followers)
+
+#+= just adds the value on the right to the variable on the left and updates the variable with the new value. In this case, it is used to increase the number of followers each day.
