673d6c22d3037ff44fbea5e7d2467c7c0a95c8e3
DonLJK <donovanlimjk18.21@gmail.com>
2026-09-04 10:31:31 +0800

    Second changes

--- Hello.py
+++ Hello.py
@@ -2,3 +2,11 @@ print("===============================")
 print("Welcome here")
 print("My first post!")
 print("===============================")
+
+username = "Cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("username: " + username)
+print("bio: " + bio)
+print("followers: " + str(followers))
