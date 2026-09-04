a93b9a877aaab55957e34f0cd14750536add55fa
DonLJK <donovanlimjk18.21@gmail.com>
2026-09-04 11:41:30 +0800

    First Post

--- Hello.py
+++ Hello.py
@@ -1,10 +1,4 @@
-followers = 100
-
-followers +=50
-print("Day 1: Followers =", followers)
-followers += 20
-print("Day 2: Followers =", followers)
-followers += 10
-print("Day 3: Followers =", followers)
-
-#+= just adds the value on the right to the variable on the left and updates the variable with the new value. In this case, it is used to increase the number of followers each day.
+print("===============================")
+print("Welcome here")
+print("My first post!")
+print("===============================")
